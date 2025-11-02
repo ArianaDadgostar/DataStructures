2c0b606890e23aa148bb60ca96ae5faf00c77955
GMR Student <gmrstudent@outlook.com>
2025-11-02 10:04:16 -0800

    Finished Radix Sort.

---
+++ Non-Comparative Sorts/Non-Comparative Sorts/RadixSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Non_Comparative_Sorts
+{
+    public class RadixSort
+    {
+        private int largest = 0;
+        private int[] buckets = new int[10];
+        private int[] sorted;
+        public void IdentifyLargestDigit(int[] array)
+        {
+            int test = array[0];
+
+            foreach(int digit in array)
+            {
+                if (digit < test) continue;
+                test = digit;
+            }
+
+            while(test > 0)
+            {
+                test /= 10;
+                largest++;
+            }
+        }
+
+        public int[] Sort(int[] array)
+        {
+            IdentifyLargestDigit(array);
+            int digit = 0;
+            
+            sorted = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sorted[i] = array[i];
+            }
+
+            for (int i = 0; i < largest; i++)
+            {
+                buckets = new int[10];
+                for(int j = 0; j < sorted.Length; j++)
+                {
+                    Math.DivRem(sorted[j], (int)Math.Pow(10.0, i + 1), out digit);
+                    digit /= (int)Math.Pow(10.0, i);
+                    buckets[digit]++;
+                }
+
+                for (int j = 1; j < buckets.Length; j++)
+                {
+                    buckets[j] += buckets[j - 1];
+                }
+
+                int[] temp = new int[sorted.Length];
+
+                for(int j = sorted.Length - 1; j >= 0; j--)
+                {
+                    Math.DivRem(sorted[j], (int)Math.Pow(10.0, i+1), out digit);
+                    digit /= (int)Math.Pow(10.0, i);
+
+                    buckets[digit]--;
+                    temp[buckets[digit]] = sorted[j];
+                }
+
+                sorted = temp;
+            }
+
+            return sorted;
+        }
+    }
+}
--- Non-Comparative Sorts/SortTest/CountingSortTest.cs
+++ Non-Comparative Sorts/SortTest/CountingSortTest.cs
@@ -35,5 +35,21 @@ namespace SortTest
                 Assert.True(sorted[i] == solution[i]);
             }
         }
+
+        [Theory]
+        [InlineData(new int[] { 34, 2, 100, 42 }, new int[] { 2, 34, 42, 100 })]
+        [InlineData(new int[] { 150, 2000, 14, 3 }, new int[] { 3, 14, 150, 2000 })]
+        [InlineData(new int[] { 19, 20, 1, 404 }, new int[] { 1, 19, 20, 404 })]
+        [InlineData(new int[] { 10, 9, 8, 7, 6, 10000, 5, 4, 3, 2, 1, 0 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10000 })]
+        public void RadixTest(int[] array, params int[] solution)
+        {
+            RadixSort sorting = new RadixSort();
+            int[] sorted = sorting.Sort(array);
+
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                Assert.True(sorted[i] == solution[i]);
+            }
+        }
     }
 }
